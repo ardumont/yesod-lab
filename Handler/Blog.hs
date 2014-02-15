32d289b802f2c03ee122280f4803b5f802247f19
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-15 09:27:29 +0100

    First part of dealing with article - create get and display input form for article

--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -1,9 +1,21 @@
-module Handler.Blog where
+module Handler.Blog (getBlogR, postBlogR) where
 
 import Import
 
+-- to use Html into forms
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+instance YesodNic App
+
+entryForm :: Form Article
+entryForm = renderDivs $ Article
+    <$> areq textField "Title" Nothing
+    <*> areq nicHtmlField "Content" Nothing
+
 getBlogR :: Handler Html
-getBlogR = error "Not yet implemented: getBlogR"
+getBlogR =
+  do articles <- runDB $ selectList [] [Desc ArticleTitle]
+     (articleWidget, enctype) <- generateFormPost entryForm
+     defaultLayout $ do $(widgetFile "articles")
 
 postBlogR :: Handler Html
 postBlogR = error "Not yet implemented: postBlogR"
